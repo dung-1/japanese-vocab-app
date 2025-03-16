cc763a7c9f3d9b1e597d0a69544b5bc483fef701
Dung Paulus CTS <linhtam2001a@gmail.com>
2025-03-16 09:54:18 +0700

    add mobille

---
+++ android/app/src/main/java/dungcts/japanese/app/MainActivity.java
@@ -0,0 +1,5 @@
+package dungcts.japanese.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dungcts.japanese.app',
+  appName: 'japanese-vocab-app',
+  webDir: 'dist/japanese-vocab-app/browser',
+};
+
+export default config;
